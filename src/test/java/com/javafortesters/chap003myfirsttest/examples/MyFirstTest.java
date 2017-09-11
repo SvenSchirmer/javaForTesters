73e0035a307e3f2ede81a30ce2a6b9fccb04328a
sven.roundhead <s.schirmer@email.de>
2017-09-11 17:13:43 +0200

    changed Unit-Test

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -15,7 +15,7 @@ public class MyFirstTest {
     @Test
     public void canAddTwoPlusTwo(){
 
-        int answer = 2+2;
-        assertEquals("2+2=4", 4, calc.addValues(2, 2) );
+        // int answer = 2+2;
+        assertEquals("2+2=4", 5, calc.addValues(3, 2) );
     }
 }
